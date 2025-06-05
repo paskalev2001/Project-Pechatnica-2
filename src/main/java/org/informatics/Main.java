6b11de275b170872acf227f9ddea78ea079fc28d
paskalev2001 <2001paskalpaskalev@gmail.com>
2025-06-05 13:53:00 +0300

    Updated main method to reflect new changes after removal of duplicated domain properties

--- src/main/java/org/informatics/Main.java
+++ src/main/java/org/informatics/Main.java
@@ -1,115 +1,224 @@
 package org.informatics;
 
-import org.informatics.domain.Publication;
-import org.informatics.service.PublicationService;
-import org.informatics.domain.PageSize;
-import org.informatics.domain.PaperType;
+import org.informatics.domain.*;
+import org.informatics.service.*;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //---- Publication Example
-//        System.out.println("------- Publication Example --------\n");
-//        Publication publication = new Publication(
-//                UUID.randomUUID().toString(),
-//                "Java Programming Basics",
-//                500,
-//                PageSize.A4,
-//                PaperType.GLOSSY,
-//                true,
-//                5.00,
-//                7000
-//        );
-//
-//        System.out.println(publication);
-//
-//        PublicationService publicationService = new PublicationService();
-//        double totalIncome = publicationService.calculateTotalIncome(publication);
-//
-//        System.out.println(publication);
-//        System.out.println("Total Income: " + totalIncome + " лв.");
+        System.out.println("------- Publication Example --------\n");
+
+        PublicationService service = new PublicationService();
+
+        Publication book = new Publication("b1", "Harry Potter and the Prisoner from Azkaban", PageSize.A4, PaperType.GLOSSY, 0, 250);
+        Publication newspaper = new Publication("n1", "Daily herald News", PageSize.A3, PaperType.NEWSPAPER, 0, 24);
+        Publication poster = new Publication("p1", "100 Kila Event", PageSize.A2, PaperType.NORMAL, 0, 2);
+
+        service.addPublication(book);
+        service.addPublication(newspaper);
+        service.addPublication(poster);
+
+        service.recalculatePricePerCopy("b1");
+        service.recalculatePricePerCopy("n1");
+        service.recalculatePricePerCopy("p1");
+
+        for (Publication pub : service.getAllPublications()) {
+            System.out.println(pub);
+            System.out.println("Price per copy: " + pub.getPricePerCopy());
+            System.out.println("---------------");
+        }
+
+        // Пример за промяна на цена на ръка (ако искаш да наложиш custom цена)
+        book.setPricePerCopy(123.45);
+        System.out.println("After setting the price per copy we get: " + book.getPricePerCopy());
+
+        // Пример за премахване на издание
+        service.removePublication("n1");
+        System.out.println("Publications after we removed the News Paper :");
+        for (Publication pub : service.getAllPublications()) {
+            System.out.println(pub);
+        }
+
+        //------ Edition Func Example --------
+        System.out.println("\n------ Edition Func Example --------\n");
+
+
+        PublicationService publicationService = new PublicationService();
+        publicationService.addPublication(book);
+        publicationService.recalculatePricePerCopy("b1");
+
+        System.out.println("Information for edition : " + book);
+        System.out.println("Price per copy: " + book.getPricePerCopy());
+
+        Edition edition = new Edition("ed1", book, 100, false);
+
+        EditionService editionService = new EditionService();
+
+        System.out.println("\n----- Printing info -----");
+        System.out.println(edition);
+
+        double costPerSheet = editionService.getCostPerSheet(edition);
+        int totalSheets = editionService.getTotalPagesPrinted(edition);
+        double totalPaperCost = editionService.getTotalPaperCost(edition);
+        double totalIncome = editionService.calculateTotalIncome(edition);
+
+        System.out.printf("Cost per sheet: %.2f lv.\n", costPerSheet);
+        System.out.println("Total sheets: " + totalSheets);
+        System.out.printf("Total paper cost: %.2f lv.\n", totalPaperCost);
+        System.out.printf("Total Income: %.2f lv.\n", totalIncome);
 
         //---- Employees Example
 
-//        System.out.println("\n------- Employees Example --------\n");
-//
-//        Employee operator = new Operator("Ivan Ivanov", 1200.0);
-//        Employee manager = new Manager("Petar Petrov", 1500.0, 10.0, 10000.0);
-//
-//        totalIncome = 10000.0;
-//
-//        System.out.println(operator);
-//        System.out.println("Salary of the operator: " + operator.calculateSalary(totalIncome));
-//
-//        System.out.println(manager);
-//        System.out.println("Salary of the manager: " + manager.calculateSalary(totalIncome));
-//
-//        //----- Employees Serializer Example
-//
-//        System.out.println("\n------- Employees Example --------\n");
-//
-//        List<Employee> employees = new ArrayList<>();
-//        employees.add(new Operator("Serial Serialov", 500.0));
-//        employees.add(new Manager("John Johnson", 1600.0, 10.0, 10000.0));
-//
-//        String filename = "employees.ser";
-//
-//        try {
-//            EmployeeSerializer.serializeEmployees(employees, filename);
-//            List<Employee> loadedEmployees = EmployeeSerializer.deserializeEmployees(filename);
-//
-//            for (Employee emp : loadedEmployees) {
-//                System.out.println(emp);
-//            }
-//
-//            EmployeeSerializer.deserializeEmployees("missingfile.ser");
-//
-//        } catch (EmployeeSerializationException e) {
-//            System.err.println(e.getMessage());
-//        }
-//
-//        //------ Edition Func Example --------
-//        System.out.println("\n------ Edition Func Example --------\n");
-//        Publication pub = new Publication("Publication of Edition", 1000, PageSize.A3, PaperType.GLOSSY, true, 1.5);
-//        Edition ed = new Edition(pub, 800, true);
-//
-//        System.out.println(ed);
-//        System.out.println("Price per sheet: " + ed.getCostPerSheet());
-//        System.out.println("Total paper cost: " + ed.getTotalPaperCost());
-//
-//        //---------- Printing House Example ------------
-//        System.out.println("\n---------- Printing House Example ------------\n");
-//        PrintingHouse house = new PrintingHouse("Bulgaria Print",
-//                500.0, 20.0, 100, 10.0);
-//
-//        house.addEmployee(new Operator("Ivan", 800));
-//        house.addEmployee(new Manager("Maria", 1000, 7, 100000));
-//
-//        PrintingMachine colorMachine = new PrintingMachine("Canon Color", true, 1000, 10);
-//        PrintingMachine bwMachine = new PrintingMachine("HP B/W", false, 1000, 15);
-//        house.addMachine(bwMachine);
-//        house.addMachine(colorMachine);
-//
-//
-//        colorMachine.loadPaper(500);
-//        bwMachine.loadPaper(300);
-//
-//        Publication newBook = new Publication("New book", 120, PageSize.A4, PaperType.NORMAL, false, 5.0);
-//        Publication poster = new Publication("Concert Poster", 60, PageSize.A2, PaperType.GLOSSY, true, 3.5);
-//        Publication pub4 = new Publication("TextBook", 30, PageSize.A4, PaperType.NORMAL, false, 1.2);
-//
-//        Edition edition4 = new Edition(pub4, 30, false);
-//        Edition edition1 = new Edition(newBook, 120, false);
-//        Edition edition2 = new Edition(poster, 60, true);
-//
-//        house.addEdition(edition1);
-//        house.addEdition(edition2);
-//        house.addEdition(edition4);
-//        house.printEdition(edition1);
-//        house.printEdition(edition2);
-//        house.printEdition(edition4);
-//
-//        house.generateReport();
+        System.out.println("\n------- Employees Example --------\n");
+
+        Employee op1 = new Operator("op1", "Ivan ivanov", 1200, "Production");
+        Employee op2 = new Operator("op2", "Petur Petrov", 1400, "Production");
+        Employee manager = new Manager("m1", "Jaki Menigera", 2000, "Management", 15, 10000);
+
+        Map<String, Employee> employees = new HashMap<>();
+        employees.put(op1.getId(), op1);
+        employees.put(op2.getId(), op2);
+        employees.put(manager.getId(), manager);
+
+        EmployeeService employeeService = new EmployeeService();
+
+        double totalIncomeEmployees = 12000;
+
+        double totalPayroll = employeeService.getTotalPayroll(employees, totalIncomeEmployees);
+
+        System.out.println("---- List of employees and their income " + totalIncomeEmployees + " ----");
+        for (Employee e : employees.values()) {
+            System.out.printf("%s: %.2f lv.%n", e.getName(), e.calculateSalary(totalIncomeEmployees));
+        }
+        System.out.printf("Total Payroll: %.2f lv.%n", totalPayroll);
+
+        String searchId = "op2";
+        Employee found = employeeService.findEmployeeById(employees, searchId);
+        if (found != null) {
+            System.out.println("Found employee: " + found);
+        }
+
+        // -------------- Printing Machine Example -------------------
+        System.out.println("-------------- Printing Machine Example -------------------");
+
+
+        PrintingMachine machine = new PrintingMachine("m1", false, 60000, 120); // 60 000 листа макс, 120 стр/мин
+
+        PrintingMachineService pmService = new PrintingMachineService();
+
+        System.out.println("Machine before being loaded:");
+        System.out.println(machine);
+
+        try {
+            int loaded = pmService.loadPaper(machine, 50000);
+            System.out.println("Loaded paper: " + loaded);
+        } catch (Exception ex) {
+            System.out.println("Error when being loaded: " + ex.getMessage());
+        }
+
+        System.out.println("Machine after being loaded:");
+        System.out.println(machine);
+
+        try {
+            pmService.printEdition(machine, edition);
+        } catch (Exception ex) {
+            System.out.println("Error while printing: " + ex.getMessage());
+        }
+
+        System.out.println("Machine state after the print:");
+        System.out.println(machine);
+        System.out.println("Total Pages Printed: " + machine.getTotalPagesPrinted());
+
+        Edition colorEdition = new Edition("ed2", book, 10, true);
+        try {
+            pmService.printEdition(machine, colorEdition);
+        } catch (Exception ex) {
+            System.out.println("Expected error when doing color print: " + ex.getMessage());
+        }
+
+        Edition bigEdition = new Edition("ed3", book, 100000, false);
+        try {
+            pmService.printEdition(machine, bigEdition);
+        } catch (Exception ex) {
+            System.out.println("Expected error when paper is not enough: " + ex.getMessage());
+        }
+
+        System.out.println("\n--- Edition that were printed on the machimnes ---");
+        machine.getPrintedEditions().forEach((ed, count) ->
+                System.out.println(ed + " - copies: " + count)
+        );
+
+        //---------- Printing House Example ------------
+        System.out.println("\n---------- Printing House Example ------------\n");
+
+        Publication publication = new Publication(
+                "book4",
+                "Java book",
+                PageSize.A4,
+                PaperType.GLOSSY,
+                0,
+                200     // 200 страници
+        );
+
+        Edition edition2 = new Edition(
+                "ed1",
+                publication,
+                100,
+                true
+        );
+
+        PrintingMachine machine2 = new PrintingMachine(
+                "pm1",
+                true,
+                30000,
+                100
+        );
+
+        PrintingMachineService machineService = new PrintingMachineService();
+
+        try {
+            int loaded = machineService.loadPaper(machine2, 20000);
+            System.out.println("Successfully loaded paper: " + loaded);
+        } catch (Exception ex) {
+            System.out.println("Error loading paper: " + ex.getMessage());
+        }
+
+        try {
+            machineService.printEdition(machine2, edition2);
+        } catch (Exception ex) {
+            System.out.println("Error while printing: " + ex.getMessage());
+        }
+
+        System.out.println(machine2);
+        System.out.println("Total pages printed: " + machine2.getTotalPagesPrinted());
+
+        System.out.println("\n----- Report for printed edition ----");
+        machine2.getPrintedEditions().forEach((ed, count) ->
+                System.out.println(ed + " - printed copies: " + count)
+        );
+
+        try {
+            machineService.loadPaper(machine2, 20000);
+        } catch (Exception ex) {
+            System.out.println("Очаквана грешка при презареждане: " + ex.getMessage());
+        }
+
+        PrintingMachine bwMachine = new PrintingMachine(
+                "pm2",
+                false,
+                10000,
+                80
+        );
+        machineService.loadPaper(bwMachine, 5000);
+        try {
+            machineService.printEdition(bwMachine, edition);
+        } catch (Exception ex) {
+            System.out.println("Expected error when printing on BW machine " + ex.getMessage());
+        }
     }
 }
